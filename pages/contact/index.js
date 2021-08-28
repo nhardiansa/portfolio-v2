417b25f57009c797f58c50629dd1aaf7de6cddd0
nhardiansa <nhardiansa@gmail.com>
2021-08-28 09:22:25 +0800

    menambahkan link ke halaman yang dituju

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import Layout from '../../components/Layout';
 import Saly14 from '../../public/img/Saly-14.png';
 
@@ -12,10 +13,31 @@ export default function Contact() {
         <div className="contact col-span-2">
           <h1 className="text-white text-5xl font-bold">You can reach me any time &#128521; </h1>
           <ul className="text-white list-none mt-12">
-            <li className="mb-4 bg-github bg-no-repeat bg-left pl-6">Github: <b>nhardiansa</b></li>
-            <li className="mb-4 bg-telegram bg-no-repeat bg-left pl-6">Telegram: <b>@matchaboy02</b></li>
-            <li className="mb-4 bg-email bg-no-repeat bg-left pl-8">Email: <b>nabilhardiansa02@gmail.com</b></li>
-            <li className="mb-4 bg-linkedin bg-no-repeat bg-left pl-6">Linkedin: <b>nabil-hardiansa</b></li>
+
+            <Link href="https://github.com/nhardiansa">
+              <a target="_blank">
+                <li className="mb-4 bg-github bg-no-repeat bg-left pl-6">
+                  Github: <b>nhardiansa</b>
+                </li>
+              </a>
+            </Link>
+
+            <Link href="https://t.me/matchaboy02">
+              <a target='_blank'>
+                <li className="mb-4 bg-telegram bg-no-repeat bg-left pl-6">Telegram: <b>@matchaboy02</b></li>
+              </a>
+            </Link>
+            <Link href="mailto:nabilhardiansa02@gmail.com">
+              <a target='_blank'>
+                <li className="mb-4 bg-email bg-no-repeat bg-left pl-8">Email: <b>nabilhardiansa02@gmail.com</b></li>
+              </a>
+            </Link>
+            <Link href="https://www.linkedin.com/in/nabil-hardiansa">
+              <a target='_blank'>
+                <li className="mb-4 bg-linkedin bg-no-repeat bg-left pl-6">Linkedin: <b>nabil-hardiansa</b></li>
+              </a>
+            </Link>
+            
           </ul>
         </div>
       </div>
