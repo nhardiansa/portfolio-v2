55f70b124aa006e86b11a1f8604bfa6865473ab5
nhardiansa <nhardiansa@gmail.com>
2021-08-28 11:32:25 +0800

    menambah project di portofolio

--- pages/portofolio/index.js
+++ pages/portofolio/index.js
@@ -4,6 +4,9 @@ import ProjectCard from '../../components/ProjectCard';
 // import Image assets here
 import OneMinuteQuiz from '../../public/img/portofolio/one_minute_quiz.png';
 import Pensiunanku from '../../public/img/portofolio/pensiunanku.png';
+import EdieHomepage from '../../public/img/portofolio/edie_homepage.png';
+import MovieGame from '../../public/img/portofolio/movie_game.png';
+import RecipeBlog from '../../public/img/portofolio/recipe_blog.png';
 
 // edit detail here
 const projects = [
@@ -21,12 +24,33 @@ const projects = [
     demo: "https://pensiunanku.netlify.app",
     focus: "top left"
   },
+  {
+    image: EdieHomepage,
+    title: "Landing Page Edie",
+    description: "This landing page is a website to introduce a team that provides web services with many benefits.",
+    demo: "https://devchallenge-edie-homepage.netlify.app",
+    focus: "top left"
+  },
+  {
+    image: MovieGame,
+    title: "Movie and Game Web App",
+    description: "This website is a learning result of ReactJS in Sanbercode bootcamp which has implemented various general concepts in React such as the use of context, authentication, components, etc.",
+    demo: "https://tugas-sanber-nabil.netlify.app",
+    focus: "top left"
+  },
+  {
+    image: RecipeBlog,
+    title: "Recipe Blog",
+    description: "This website is the result of the slicing challenge from devchallenge.",
+    demo: "https://nab-recipe-blog.netlify.app",
+    focus: "top center"
+  },
 ];
 
 export default function Portofolio() {
   return (
     <Layout page="Portofolio">
-      <div className="container pt-16 px-7 min-h-[80vh] md:min-h-[90vh] md:px-24 md:pt-20">
+      <div className="container pt-16 px-7 min-h-[80vh] md:min-h-[90vh] md:px-24 md:py-20">
         <h1 className="font-black text-white text-center text-3xl md:text-4xl">What I've done &#x1F60B;</h1>
         <div className="cards mt-12 md:grid grid-cols-2 gap-8">
           {
