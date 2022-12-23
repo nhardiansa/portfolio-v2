1697fdb80f49d105bf7537ac264e4b1e98a7252e
Nabil Hardiansa <nhardiansa@gmail.com>
2022-12-23 21:23:06 +0700

    fix: behaviour dev env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
   images: {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    domains: ["i.ibb.co"],
+    domains: ['i.ibb.co'],
   },
 };
+
+module.exports = nextConfig;
