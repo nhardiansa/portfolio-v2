8995ea071c23d45ab20daa97681cebbea6beb199
Nabil Hardiansa <nhardiansa@gmail.com>
2022-12-23 20:40:48 +0700

    BREAKING CHANGE!: upgrade next and tailwind version then add typescript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,42 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       animation: {
-        'bounce-short': 'mantul 1s infinite'
+        'bounce-short': 'mantul 1s infinite',
       },
       keyframes: {
         mantul: {
           '0%, 100%': {
-            transform: 'translateY(-5%)',
+            'transform': 'translateY(-5%)',
             'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)',
           },
           '50%': {
-            transform: 'translateY(0)',
+            'transform': 'translateY(0)',
             'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)',
           },
         },
       },
-      backgroundImage: theme => ({
-        'github' : "url('/icons/github.svg')",
-        'telegram' : "url('/icons/telegram.svg')",
-        'linkedin' : "url('/icons/linkedin.svg')",
-        'email' : "url('/icons/email.svg')",
+      backgroundImage: (theme) => ({
+        github: 'url(\'/icons/github.svg\')',
+        telegram: 'url(\'/icons/telegram.svg\')',
+        linkedin: 'url(\'/icons/linkedin.svg\')',
+        email: 'url(\'/icons/email.svg\')',
       }),
     },
     container: {
       center: true,
     },
     fontFamily: {
-      'inter': 'Inter, sans-serif',
-      'coba': 'Scheherazade, serif',
+      inter: 'Inter, sans-serif',
+      coba: 'Scheherazade, serif',
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
+  // eslint-disable-next-line eol-last
 };
